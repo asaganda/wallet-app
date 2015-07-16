e791301a0a0a346f15d75de539449ebb5d4eb27f
Andrew <Asaganda@gmail.com>
2015-07-16 15:51:52 -0400

    Add phone column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716181324) do
+ActiveRecord::Schema.define(version: 20150716193132) do
 
   create_table "cards", force: :cascade do |t|
     t.string   "number"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150716181324) do
     t.decimal  "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "phone"
   end
 
 end
