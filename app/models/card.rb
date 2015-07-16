5e9424d761f4d8691301ced16dab2f91e5b35ff1
Andrew <Asaganda@gmail.com>
2015-07-16 16:54:12 -0400

    Validations for models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,38 @@
 class Card < ActiveRecord::Base
+
+  # \A represents the beginning of a string,[345] in bracket acts as big OR statement.. 3 or 4 or 5
+  # \d represents the number of digits (in our case, the length),{11,} is 11 and above
+  # \Z represents the end of the string
+  # checkout rubular.com for reference
+  validates_format_of :number, with: /\A[345]\d{11,}/
+
+  # expiration month can be between month 1 and month 12
+  validates :expiration_month, inclusion: {in: (1..12)}
+
+  # expiration year can be between year 2015 and 2115
+  validates :expiration_year, inclusion: {in: (2015..2115)}
+
+  # this saves the card type before the card is saved into database
+  before_save :set_card_type
+
+  
+  def set_card_type
+    # use self. for activerecord instance
+    first_num = self.number[0]
+
+    # since we're setting type for this card use self.
+      # we have to use self.type otherwitse it will set the type as a variable
+    self.type = case first_num
+
+    # first_num is in a string..so has to be '3'
+    when '3'
+      'American Express'
+    when '4'
+      'Visa'
+    when '5'
+      'MasterCard'
+    end
+  end
   # many to many relationship
   has_many :user_cards
   has_many :users, through: :user_cards
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
+  # firstname, lastname, and email all must be present 
+  # for a user to be valid
+  validates_presence_of :fname, :lname, :email
+  # tests that the email provided by user is unique and 
+  # that the database doesn't have that email already 
+  # (no account previously made)
+  validates_uniqueness_of :email
+  #we want the user to input their phone number with dashes
+  validates_format_of :phone, with: /\d{3}-\d{3}-\d{4}/
+  # the account balance must be greater than 0.00
+  validates_numerically_of :balance, { greater_than: 0}
+
   # many to many relationship
   has_many :user_cards
   has_many :card, through: :user_cards
